77555d255eb7979d31afdce86e773d17ceae9ff2
김동국 <dongkuk97@naver.com>
2022-11-11 16:55:21 +0900

    1. selector list 출력 완성

--- src/components/SectionSelector.vue
+++ src/components/SectionSelector.vue
@@ -2,28 +2,44 @@
   <div>
     <div class="area_select_container">
       <div class="form_group">
-        <select class="form_select" name="sido" id="sido">
+        <select class="form_select" name="sido" id="sido" v-model="sidoVal">
+          <!-- Todo: focus가면 시도선택 없어지게 하기 -->
           <option value="">시도선택</option>
+          <option :value="item" v-for="(item, index) in sidoList" :key="index">
+            {{ item }}
+          </option>
         </select>
       </div>
       <div class="form_group">
-        <select class="form_select" name="gugun" id="gugun">
+        <select class="form_select" name="gugun" id="gugun" v-model="gugunVal">
           <option value="">구군선택</option>
+          <option :value="item" v-for="(item, index) in gugunList" :key="index">
+            {{ item }}
+          </option>
         </select>
       </div>
       <div class="form_group">
-        <select class="form_select" name="dong" id="dong">
+        <select class="form_select" name="dong" id="dong" v-model="dongVal">
           <option value="">동선택</option>
+          <option :value="item" v-for="(item, index) in dongList" :key="index">
+            {{ item }}
+          </option>
         </select>
       </div>
       <div class="form_group">
-        <select class="form_select" name="year" id="year">
+        <select class="form_select" name="year" id="year" v-model="yearVal">
           <option value="">매매년도선택</option>
+          <option :value="item" v-for="(item, index) in yearList" :key="index">
+            {{ item }} 년
+          </option>
         </select>
       </div>
       <div class="form_group">
         <select class="form_select" name="month" id="month">
           <option value="">매매월선택</option>
+          <option :value="item" v-for="(item, index) in monthList" :key="index">
+            {{ item }} 월
+          </option>
         </select>
       </div>
       <div class="form_group">
@@ -39,8 +55,75 @@
 </template>
 
 <script>
+import http from "@/util/http-common";
+
 export default {
   name: "SectionSelector",
+  data() {
+    return {
+      sidoList: [],
+      gugunList: [],
+      dongList: [],
+      sidoVal: "",
+      gugunVal: "",
+      dongVal: "",
+      yearList: [],
+      monthList: [],
+      yearVal: "",
+      monthVal: "",
+    };
+  },
+  mounted() {
+    this.getSido();
+    this.getYear();
+    this.getMonth();
+  },
+  methods: {
+    getSido: async function () {
+      const subUrl = "search/sido";
+      let resSido = await http.get(`${subUrl}`);
+      this.sidoList = resSido.data.data;
+    },
+    getYear: function () {
+      let date = new Date();
+      let year = date.getFullYear();
+      for (let i = year; i > year - 10; i--) {
+        this.yearList.push(i);
+      }
+    },
+    // getMonth: function () {
+    //   let date = new Date();
+    //   let year = date.getFullYear();
+    //   for (let i = year; i > year - 10; i--) {
+    //     this.yearList.push(i);
+    //   }
+    // },
+  },
+  watch: {
+    sidoVal: async function (sidoVal) {
+      console.log(sidoVal);
+      const subUrl = "search/gugun";
+      const reqData = { sido: sidoVal };
+      let resGugun = await http.get(`${subUrl}`, { params: reqData });
+      this.gugunList = resGugun.data.data;
+    },
+    gugunVal: async function (gugunVal) {
+      const subUrl = "search/dong";
+      const reqData = { sido: this.sidoVal, gugun: gugunVal };
+      console.log(reqData);
+      let resDong = await http.get(`${subUrl}`, { params: reqData });
+      this.dongList = resDong.data.data;
+    },
+    yearVal: function () {
+      let date = new Date();
+      let month = date.getMonth() + 1;
+      let m = this.yearVal === date.getFullYear() ? month : 13;
+      for (let i = 1; i < m; i++) {
+        // let temp = i < 10 ? "0" + i : i;
+        this.monthList.push(i);
+      }
+    },
+  },
 };
 </script>
 
