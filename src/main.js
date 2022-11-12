29b3661aebea7df8f82cfbc14a13fefb99211d4d
Gukss <dongkuk97@naver.com>
2022-11-12 22:45:52 +0900

    1. store layout

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueSession from "vue-session";
+import { store } from "./store";
 
 var sessionOptions = {
   persist: true,
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store: store,
   render: (h) => h(App),
 }).$mount("#app");
