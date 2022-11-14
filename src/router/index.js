39d554560ecd2dbfb7201992d3c5d72ee5ecdff6
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2022-11-14 17:37:27 +0900

    import 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import IndexView from "@/views/IndexView.vue";
 import SearchView from "@/views/SearchView.vue";
+import store from "@/store/store.js";
 
 Vue.use(VueRouter);
 
