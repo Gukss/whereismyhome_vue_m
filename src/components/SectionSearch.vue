ef2187d0a5d04fa4dece6ac1db37834cb27653df
김동국 <dongkuk97@naver.com>
2022-11-14 13:25:20 +0900

    1. 마커 추가하고, 마커가 다 보이는 level로 map 출력하기

--- src/components/SectionSearch.vue
+++ src/components/SectionSearch.vue
@@ -70,6 +70,42 @@ export default {
         level: 4,
       };
       this.map = new kakao.maps.Map(container, options);
+      /* =========================== */
+      /* =    지도 컨트롤러 추가   = */
+      /* =========================== */
+      // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+      let mapTypeControl = new kakao.maps.MapTypeControl();
+      // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+      // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+      this.map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPLEFT);
+      // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+      var zoomControl = new kakao.maps.ZoomControl();
+      this.map.addControl(zoomControl, kakao.maps.ControlPosition.LEFT);
+      /* =========================== */
+      /* =     지도 범위 설정      = */
+      /* =========================== */
+      // 지도를 재설정할 범위정보를 가지고 있을 LatLngBounds 객체를 생성합니다
+      let bounds = new kakao.maps.LatLngBounds();
+      // 버튼을 클릭하면 아래 배열의 좌표들이 모두 보이게 지도 범위를 재설정합니다
+      let points = [];
+      for (let i = 0; i < this.$store.getters.getAptList.length; i++) {
+        points.push(
+          new kakao.maps.LatLng(
+            this.$store.getters.getAptList[i].lat,
+            this.$store.getters.getAptList[i].lng
+          )
+        );
+      }
+      let i, marker;
+      for (i = 0; i < this.$store.getters.getAptList.length; i++) {
+        // 배열의 좌표들이 잘 보이게 마커를 지도에 추가합니다
+        marker = new kakao.maps.Marker({ position: points[i] });
+        marker.setMap(this.map);
+
+        // LatLngBounds 객체에 좌표를 추가합니다
+        bounds.extend(points[i]);
+      }
+      this.map.setBounds(bounds);
     },
   },
 };
