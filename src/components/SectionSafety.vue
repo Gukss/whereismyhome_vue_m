08e8fe8791276a8ee55cda2ddbe398913ab5a033
Gukss <dongkuk97@naver.com>
2022-11-19 16:46:09 +0900

    안심하우스 페이지

---
+++ src/components/SectionSafety.vue
@@ -0,0 +1,136 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="banner_container">
+        <div class="banner_info">
+          <div class="banner_title_container">
+            <img src="@/assets/img/jam_home.svg" alt="aa" />
+            <div class="banner_text_container">
+              <div class="title">안심하우스</div>
+              <div class="subtitle">안심하우스를 찾아보세요.</div>
+            </div>
+          </div>
+        </div>
+        <div class="banner_info">
+          <div class="card_container">
+            <div class="card">
+              <img class="medal" src="@/assets/img/SilverMedal.svg" alt="silver" />
+              <div class="card_table_container">
+                <table class="card_table">
+                  <colgroup>
+                    <col width="35%" />
+                    <col width="65%" />
+                  </colgroup>
+                  <tr>
+                    <td class="title">아파트이름</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">주소</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">면적</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">최근거래금액</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">가로등개수</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">CCTV개수</td>
+                    <td>AAAAA</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="card">
+              <img class="medal" src="@/assets/img/GoldMedal.svg" alt="gold" />
+              <div class="card_table_container">
+                <table class="card_table">
+                  <colgroup>
+                    <col width="35%" />
+                    <col width="65%" />
+                  </colgroup>
+                  <tr>
+                    <td class="title">아파트이름</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">주소</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">면적</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">최근거래금액</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">가로등개수</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">CCTV개수</td>
+                    <td>AAAAA</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div class="card">
+              <img class="medal" src="@/assets/img/BronzeMedal.svg" alt="silver" />
+              <div class="card_table_container">
+                <table class="card_table">
+                  <colgroup>
+                    <col width="35%" />
+                    <col width="65%" />
+                  </colgroup>
+                  <tr>
+                    <td class="title">아파트이름</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">주소</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">면적</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">최근거래금액</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">가로등개수</td>
+                    <td>AAAAA</td>
+                  </tr>
+                  <tr>
+                    <td class="title">CCTV개수</td>
+                    <td>AAAAA</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SectionSafety",
+};
+</script>
+
+<style scoped>
+@import "@/assets/css/SectionSafety.css";
+</style>
